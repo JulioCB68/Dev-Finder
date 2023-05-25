9c81e24a7b2616244163078830cb269138391001
Júlio César <juliobento237@gmail.com>
2023-05-24 22:40:12 -0300

    (setup): configuring ESLint and Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import { robotoMono } from "@/config/font";
-import { metaData } from "@/config/meta";
 import Head from "next/head";
 
-export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>{metaData.title}</title>
-				<meta name="description" content={metaData.description} />
-			</Head>
+import { robotoMono } from "@/config/font";
+import { metaData } from "@/config/meta";
 
-			<div className={`${robotoMono.className}`}>
-				<h1>Hello World!</h1>
-			</div>
-		</>
-	);
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{metaData.title}</title>
+        <meta name="description" content={metaData.description} />
+      </Head>
+
+      <div className={`${robotoMono.className}`}>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
 }
